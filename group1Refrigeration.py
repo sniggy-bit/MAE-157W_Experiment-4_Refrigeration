ddd3e94b9630dce630a5461bcfe7ba8b9fbf66b7
andrewyzhuang <65326843+andrewyzhuang@users.noreply.github.com>
2025-05-10 23:08:03 -0700

    comments

--- group1Refrigeration.py
+++ group1Refrigeration.py
@@ -15,29 +15,6 @@ columns = [
     "Evap [A]", "Comp [A]", "Cond [A]", "Voltage [V]", "Flow Rate [lb/min]"
 ]
 
-'''
-# Function to clean and convert one dataset
-def clean_data(file):
-    df = pd.read_csv(file, header=None, names=columns)
-
-    # Convert temperatures to Kelvin
-    for col in df.columns:
-        if '[C]' in col:
-            df[col.replace('[C]', '[K]')] = df[col] + 273.15
-
-    # Convert pressures to Pascals
-    for col in df.columns:
-        if '[psi]' in col:
-            df[col.replace('[psi]', '[Pa]')] = df[col] * 6894.76
-
-    return df
-
-
-# Clean and load all three datasets into pandas DataFrames
-df_axv = clean_data(file_axv)
-df_txv = clean_data(file_txv)
-df_ctv = clean_data(file_ctv)
-'''
 
 # Function to clean and convert one dataset
 def clean_data(file, config_type):
